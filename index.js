1e7386c475438ffa3f24474a74e30cdeb5cd79f1
Kurosakicoder <khaidemsandipsingha@gmail.com>
2021-06-23 17:59:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function main(arr){
+    let sum = 0
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i]
+    } 
+
+    return sum;
+}
+
+let arr = [1,2,3,4,5]
+
+console.log(main(arr))
